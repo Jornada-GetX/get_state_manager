443afcb5b890d3cc3be23528083e1d8a753d6320
renatorv <renatorv@gmail.com>
2023-09-27 16:19:35 -0300

    primeiro commit

---
+++ android/app/src/main/kotlin/br/com/academiadoflutter/jornadagetx/get_state_manager/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.academiadoflutter.jornadagetx.get_state_manager
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_state_manager/basico/reatividade_page.dart';
+import 'package:get_state_manager/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+      getPages: [
+        GetPage(
+          name: '/',
+          page: () => const HomePage(),
+        ),
+        GetPage(
+          name: '/basico',
+          page: () => ReatividadePage(),
+        ),
+      ],
+    );
+  }
+}
